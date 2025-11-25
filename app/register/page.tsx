b4cc9544bb7065816ba22f4aa6fc35a4767bef9f
BretRen <bretren2013@gmail.com>
2025-11-25 02:10:37 +0000

    Add Register and Login and update Home Pages

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="w-full h-screen flex justify-center items-center bg-gray-200">
       <div className="bg-white p-20 flex justify-center items-center rounded-2xl">
-        <h1 className="font-bold">Home</h1>
+        <h1 className="font-bold">Register</h1>
       </div>
     </div>
   );
