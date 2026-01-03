965cca3580c2f36d730b37f558b406cd93621500
Bret <Bretren2013@gmail.com>
2026-01-02 20:58:36 -0800

    Add change email

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,9 @@
 	import { Realtime } from 'appwrite';
 	import { goto } from '$app/navigation';
 
-	let isLoading = $state(true);
+	let isInit = $state(true);
 	let user = $state<any>({});
+  
 
 	let unverifiedEmailModal: HTMLDialogElement | null = $state(null);
 
@@ -13,7 +14,7 @@
 		try {
 			user = await account.get();
 			// console.log(user);
-			isLoading = false;
+			isInit = false;
 		} catch (error) {
 			goto('/login');
 			console.error('Error fetching user data:', error);
@@ -103,6 +104,45 @@
 		logoutInProgress = false;
 		goto('/login');
 	};
+
+  // Update Email
+  let updateEmailModal: HTMLDialogElement | null = $state(null);
+  let updateEmailError = $state('');
+  let updateEmailEmail = $state('');
+  let updateEmailPassword = $state('');
+  let updateEmailInProgress = $state(false);
+
+  const updateEmail = async () => {
+    updateEmailInProgress = true;
+    updateEmailError = '';
+    if (!updateEmailEmail || !updateEmailPassword) {
+      updateEmailError = 'Please provide both email and password.';
+      updateEmailModal?.showModal();
+      updateEmailInProgress = false;
+      return;
+    }else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(updateEmailEmail)) {
+      updateEmailError = 'Please provide a valid email address.';
+      updateEmailModal?.showModal();
+      updateEmailInProgress = false;
+      return;
+    }
+
+    try {
+      await account.updateEmail({
+        email: updateEmailEmail,
+        password: updateEmailPassword
+      });
+			user = await account.get();
+    }catch(error: any) {
+      console.error('Error updating email:', error);
+      updateEmailModal?.showModal();
+      updateEmailError = error.message || 'An error occurred while updating the email.';
+    }
+    updateEmailInProgress = false;
+  };
+
+
+
 </script>
 
 <svelte:head>
@@ -110,12 +150,21 @@
 	<meta name="description" content="Pdnode Dashboard" />
 </svelte:head>
 
+{#if updateEmailInProgress}
+<div class="toast toast-top toast-center">
+  <div class="alert alert-info alert-soft">
+    <span>We are updating your email address, which may take some time. Please do not close the webpage in the meantime.</span>
+  </div>
+</div>
+{/if}
+
 <main class="min-h-screen bg-base-200/50">
-	{#if isLoading}
+	{#if isInit}
 		<div class="flex justify-center items-center h-screen">
 			<span class="loading loading-spinner loading-lg"></span>
 		</div>
 	{:else}
+    <!-- Modal - Show Unverified Email -->
 		<dialog id="showUnverifiedEmail" class="modal" bind:this={unverifiedEmailModal}>
 			<div class="modal-box flex flex-col items-center">
 				<h3 class="text-lg font-bold text-center">Unverified Email</h3>
@@ -128,18 +177,69 @@
 						<span class="text-center">{verifyError}</span>
 					</div>
 				{/if}
-				<button class="btn" onclick={() => verifyEmail()} disabled={verifyButtonDisabled}>
-					{#if verifyButtonLoading}
-						<span class="loading loading-dots loading-md"></span>
-					{:else if verifyButtonRetryTimeLeft > 0}
-						Resend in {verifyButtonRetryTimeLeft}s
-					{:else}
-						Send Verification Email
-					{/if}
-				</button>
+
+        <div class="flex flex-row gap-4">        
+          <button class="btn" onclick={() => verifyEmail()} disabled={verifyButtonDisabled}>
+            {#if verifyButtonLoading}
+              <span class="loading loading-dots loading-md"></span>
+            {:else if verifyButtonRetryTimeLeft > 0}
+              Resend in {verifyButtonRetryTimeLeft}s
+            {:else}
+              Send Verification Email
+            {/if}
+          </button>
+          <button class="btn" onclick={() => updateEmailModal?.showModal()}>Change My Email</button>
+        </div>
 			</div>
 		</dialog>
 
+        <!-- Modal - Show Update Email Form -->
+    <dialog class="modal" bind:this={updateEmailModal}>
+      <div class="modal-box flex flex-col items-center justify-center gap-6"> <h3 class="text-lg font-bold">Update Email</h3>
+
+        <div class="flex flex-col gap-1.5 w-full max-w-xs"> 
+          <label class="text-xs font-medium opacity-70 ml-1" for="new-email">
+            Your new email
+          </label>
+          
+          <label class="input validator w-full">
+            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor">
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              </g>
+            </svg>
+            <input id="new-email" type="email" bind:value={updateEmailEmail} placeholder="mail@site.com" required />
+          </label>
+        </div>
+
+        <div class="flex flex-col gap-1.5 w-full max-w-xs">
+          <label class="text-xs font-medium opacity-70 ml-1" for="password">
+            Confirm Password
+          </label>
+          <label class="input validator w-full">
+            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor">
+                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
+            <input id="password" bind:value={updateEmailPassword} type="password" required placeholder="Password" minlength="8" />
+          </label>
+        </div>
+
+        {#if updateEmailError}
+          <div role="alert" class="alert alert-error alert-soft w-full flex justify-center items-center">
+            <span class="text-center">{updateEmailError}</span>
+          </div>
+        {/if}
+
+        <form method="dialog" class="modal-action w-full flex justify-center">
+          <button class="btn btn-primary" onclick={() => updateEmail()}>Update</button>
+          <button class="btn">Cancel</button>
+        </form>
+      </div>
+    </dialog>
 		<div class="container mx-auto px-4 pt-16 pb-12 relative z-10 max-w-5xl">
 			<div class="grid grid-cols-1 lg:grid-cols-12 gap-6">
 				<div class="lg:col-span-4">
@@ -193,7 +293,7 @@
 										</p>
 										<p class="font-medium text-lg text-base-content">{user.email}</p>
 									</div>
-									<button class="btn btn-primary btn-sm px-6">Edit</button>
+									<button class="btn btn-primary btn-sm px-6" onclick={() => updateEmailModal?.showModal()}>Edit</button>
 								</div>
 
 								<div
